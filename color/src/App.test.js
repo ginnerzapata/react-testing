9cbdf1d22f7471fdbf3d01632e4db1b7443ce31c
Ginner Zapata <gzapata@vizzn.ca>
2022-04-24 11:40:48 -0300

    added test for disbled button

--- color/src/App.test.js
+++ color/src/App.test.js
@@ -1,8 +1,36 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import {render, screen, fireEvent} from '@testing-library/react'
+import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('Button has correct inital color', () => {
+  render(<App/>)
+
+  // find an element with a role of button and text of 'Change to blue'
+  const colorButton = screen.getByRole('button', {name: 'Change to blue'})
+
+  //expects the background to be red
+  expect(colorButton).toHaveStyle({backgroundColor: 'red'})
+
+  //click button
+  fireEvent.click(colorButton)
+
+  //expects the background to be blue
+  expect(colorButton).toHaveStyle({backgroundColor:'blue'})
+
+  //expects the button to say Change to red
+
+  expect(colorButton.textContent).toBe('Change to red')
+
+})
+
+test('button disabled on checkbox checked', () => {
+  render(<App/>)
+  const button = screen.getByRole('button', {name: 'Change to blue'})
+  const checkbox = screen.getByRole('checkbox')
+  expect(button).toBeEnabled()
+
+  fireEvent.click(checkbox)
+  expect(button).toBeDisabled()
+
+  fireEvent.click(checkbox)
+  expect(button).toBeEnabled()
+})
