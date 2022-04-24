9394d1b4bfe6a4e3b3e651576f58076d15185d10
Ginner Zapata <gzapata@vizzn.ca>
2022-04-24 13:33:52 -0300

    updated for unit testing

--- color/src/App.test.js
+++ color/src/App.test.js
@@ -1,30 +1,30 @@
 import {render, screen, fireEvent} from '@testing-library/react'
-import App from './App'
+import App, { replaceCamelWithSpaces } from './App'
 
 test('Button has correct inital color', () => {
   render(<App/>)
 
   // find an element with a role of button and text of 'Change to blue'
-  const colorButton = screen.getByRole('button', {name: 'Change to blue'})
+  const colorButton = screen.getByRole('button', {name: 'Change to Midnight Blue'})
 
   //expects the background to be red
-  expect(colorButton).toHaveStyle({backgroundColor: 'red'})
+  expect(colorButton).toHaveStyle({backgroundColor: 'MediumVioletRed'})
 
   //click button
   fireEvent.click(colorButton)
 
   //expects the background to be blue
-  expect(colorButton).toHaveStyle({backgroundColor:'blue'})
+  expect(colorButton).toHaveStyle({backgroundColor:'MidnightBlue'})
 
   //expects the button to say Change to red
 
-  expect(colorButton.textContent).toBe('Change to red')
+  expect(colorButton.textContent).toBe('Change to Medium Violet Red')
 
 })
 
 test('button disa bled on checkbox checked', () => {
   render(<App/>)
-  const button = screen.getByRole('button', {name: 'Change to blue'})
+  const button = screen.getByRole('button', {name: 'Change to Midnight Blue'})
   const checkbox = screen.getByRole('checkbox', {name: 'Disable button'})
   expect(button).toBeEnabled()
 
@@ -37,13 +37,25 @@ test('button disa bled on checkbox checked', () => {
 
 test('use web color gray', () => {
   render(<App />)
-  const button = screen.getByRole('button', {name: 'Change to blue'})
+  const button = screen.getByRole('button', {name: 'Change to Midnight Blue'})
   const checkbox = screen.getByRole('checkbox', {name: 'Disable button'})
   fireEvent.click(checkbox)
   expect(button).toBeDisabled()
   expect(button).toHaveStyle({backgroundColor: "gray"})
   fireEvent.click(checkbox)
   expect(button).toBeEnabled()
-  expect(button).toHaveStyle({backgroundColor: "red"})
+  expect(button).toHaveStyle({backgroundColor: "MediumVioletRed"})
 
+})
+
+describe('spaces before camel-case capital letters', () => {
+  test('works for no inner capital letter',  () => {
+    expect(replaceCamelWithSpaces('Red')).toBe('Red')
+  })
+  test('works for one capital letter', () => {
+    expect(replaceCamelWithSpaces('MidnightBlue')).toBe('Midnight Blue')
+  })
+  test('works for multilple inner capital letters', () => {
+    expect(replaceCamelWithSpaces('MediumVioletRed')).toBe('Medium Violet Red')
+  })
 })
