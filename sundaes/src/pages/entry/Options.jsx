a7757b00f540d36fe37db380d0641cc698857967
Ginner Zapata <gzapata@vizzn.ca>
2022-05-02 05:40:43 -0300

    test for options

---
+++ sundaes/src/pages/entry/Options.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from 'react'
+import axios from "axios"
+import { ScoopOption } from './ScoopOption'
+import Row from "react-bootstrap/Row"
+
+export const Options = ({ optionType }) => {
+    const [items, setItems] = useState([])
+
+    useEffect(() => {
+        axios.get(`http://localhost:3030/${optionType}`)
+            .then(response => setItems(response.data))
+        .catch(error => {})
+    }, [optionType])
+
+    const ItemComponent = optionType === "scoops" ? ScoopOption : null
+    const optionItems = items.map(item => <ItemComponent key={item.name} name={item.name} imagePath={item.imagePath}/>)
+  return (
+      <Row>{optionItems}</Row>
+  )
+}
