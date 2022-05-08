8a3763659020c350368fda03403cf0771b015eff
Ginner Zapata <gzapata@vizzn.ca>
2022-05-08 19:59:24 -0300

    added order entry testing

--- sundaes/src/pages/entry/Options.jsx
+++ sundaes/src/pages/entry/Options.jsx
@@ -3,15 +3,23 @@ import axios from "axios"
 import { ScoopOption } from './ScoopOption'
 import { ToppingOption } from './ToppingOption'
 import Row from "react-bootstrap/Row"
+import { AlertBanner } from '../common/AlertBanner'
 
 export const Options = ({ optionType }) => {
     const [items, setItems] = useState([])
+    const [error, setError] = useState(false)
 
     useEffect(() => {
         axios.get(`http://localhost:3030/${optionType}`)
             .then(response => setItems(response.data))
-        .catch(error => {})
+            .catch(error => {
+            setError(true)
+        })
     }, [optionType])
+
+    if (error) {
+        return <AlertBanner/>
+    }
 
     const ItemComponent = optionType === "scoops" ? ScoopOption : ToppingOption
     const optionItems = items.map(item => <ItemComponent key={item.name} name={item.name} imagePath={item.imagePath}/>)
